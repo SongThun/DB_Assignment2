ae4dbdd1b4a21c02d4b21399fce36745d21e6ec5
ThunThun <120293608+SongThun@users.noreply.github.com>
2024-04-18 11:52:26 +0700

    Delete server/models directory

--- server/models/Rental.js
+++
@@ -1,9 +0,0 @@
-import db from '../database.js'
-
-const Rental = {
-  find: (values, callback) => {
-    let sql = `CALL court_availability(?,?,?); `
-    db.query(sql, values, callback);
-  }
-}
-export default Rental;
