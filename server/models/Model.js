c396d80b6207241927db6a4e44dc401b53bbcd02
ThunThun <120293608+SongThun@users.noreply.github.com>
2024-04-18 12:20:06 +0700

    Add files via upload

---
+++ server/models/Model.js
@@ -0,0 +1,121 @@
+import db from '../database.js'
+
+
+const Model = {
+  getAttributes: async function(table) {
+    let sql = `SELECT column_name, data_type
+              FROM information_schema.columns
+              WHERE table_schema = 'badminton_court_db'
+              AND table_name = '${table}'
+              ORDER BY ordinal_position;`;
+    let attributes = {};
+    const result = await db.query(sql);
+    result[0].forEach(row => {
+      attributes[row.COLUMN_NAME] = row.DATA_TYPE;
+    });
+    return attributes;
+  },
+  getPrimaryKeys: async function(table) {
+    let sql = `SELECT column_name
+              FROM information_schema.columns
+              WHERE column_key = "PRI" 
+              AND table_schema = 'badminton_court_db'
+              AND table_name = '${table}'
+              ORDER BY ordinal_position`;
+    let primaryKeys = [];
+    const result = await db.query(sql);
+    result[0].forEach(row => {
+      primaryKeys.push(row.COLUMN_NAME);
+    });
+    return primaryKeys;
+  },
+  display: async function(table, attrFilter, attrSort) {
+    const attributes = await this.getAttributes(table);
+    let sql = `SELECT `
+    for(const key in attributes) {
+      if (attributes[key] == 'date') {
+        sql += `DATE_FORMAT(${key}, '%Y-%m-%d') as ${key},`
+      }
+      else if (attributes[key] == 'time') {
+        sql += `DATE_FORMAT(${key}, '%H:%i') as ${key},`
+      }
+      else {
+        sql += `${key},`;
+      }
+    } 
+    sql = sql.slice(0, -1) + ` FROM ${table} `;
+    if (attrFilter) {
+      sql += 'WHERE ';
+      for (const key in attrFilter) {
+        sql += key + " IN (";
+        for (let i = 0; i < attrFilter[key].length; i++) {
+          sql += `'${attrFilter[key][i]}'` + ","
+        }
+        sql = sql.slice(0,-1) + ') AND '; 
+      }
+      sql = sql.slice(0,-4);
+    }
+    if (Object.keys(attrSort).length > 0) {
+      sql += ' ORDER BY ';
+      for (const key in attrSort) {
+        sql += key;
+        sql += (attrSort[key] ? ' DESC,' : ' ASC,');
+      }
+      sql = sql.slice(0,-1);
+    }
+    const result = await db.query(sql);
+    return result[0];
+  },
+  
+  create: async function(table, values) {
+    let sql = `INSERT INTO ${table}(`;
+    for (const key in values) {
+      sql += `${key},`
+    }
+    sql = sql.slice(0,-1) + ') VALUES (';
+    for (const key in values) {
+      sql += `'${values[key]}',`;
+    }
+    sql = sql.slice(0,-1) + ');';
+    try {
+      const result = await db.query(sql);
+      return {result : result[0]};
+    } catch(err) {
+      return {err: err}
+    }
+  },
+
+  update: async function(table, primaryKeys, newValues) {
+    let sql = `UPDATE ${table} SET `;
+    for (const key in newValues) {
+      sql += key + '=' + `'${newValues[key]}'` + ",";
+    }
+    sql = sql.slice(0,-1) + ' WHERE ';
+    for (const pk in primaryKeys) {
+      sql += `${pk} = '${primaryKeys[pk]}' AND `;
+    }
+    sql = sql.slice(0, -4);
+    try {
+      const result = await db.query(sql);
+      return {result : result[0]};
+    } catch(err) {
+      return {err: err}
+    }
+  },
+
+  delete: async function(table, primaryKeys) {
+    let sql = `DELETE FROM ${table} WHERE `;
+    for (const pk in primaryKeys) {
+      sql += `${pk} = '${primaryKeys[pk]}' AND `;
+    }
+    sql = sql.slice(0, -4);
+    try {
+      const result = await db.query(sql);
+      return {result : result[0]};
+    } catch(err) {
+      return {err: err}
+    }
+  }
+};
+
+export default Model;
