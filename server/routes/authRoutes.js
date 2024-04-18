587a43c14d0876e2ff75b6010348430b9b35078b
ThunThun <120293608+SongThun@users.noreply.github.com>
2024-04-18 11:52:57 +0700

    Delete server directory

--- server/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import authController from '../controllers/authController.js';
-
-const router = express.Router();
-
-router.post('/login', authController.login);
-router.post('/logout', authController.logout);
-
-export default router;
