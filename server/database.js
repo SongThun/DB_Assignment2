f0efaad9a58023639c38554de98ec297979814d0
ThunThun <120293608+SongThun@users.noreply.github.com>
2024-04-20 21:33:09 +0700

    20/04/2024 database update
    Adding database connection validation

--- server/database.js
+++ server/database.js
@@ -1,10 +1,28 @@
 import mysql from 'mysql2'
 
-const db = mysql.createPool({
-    user: 'root',
-    host: 'localhost',
-    password: '206206',
-    database: 'badminton_court_db'
-}).promise();
+let db = null;
 
-export default db;
+const createDatabase = async (username, password) => {
+    if (db) db.end((err) => console.log(err));
+
+    db = mysql.createPool({
+        user: username,
+        host: 'localhost',
+        password: password, 
+        database: 'badminton_court_db'
+    }).promise()
+
+    try {
+        const result = await db.query('select court_id from court');
+        return 'success'
+    }
+    catch(err) {
+        return err;
+    }
+    
+}
+const connect = () => {
+    return db;
+}
+
+export {connect, createDatabase}
